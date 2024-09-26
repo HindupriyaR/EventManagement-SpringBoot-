0e8e4d8609c945b23cf22b9a076f79a87f90d45a
Hindu.priya <hindu.priya@swinkpay-fintech.com>
2024-09-26 15:23:41 +0530

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        //print statement
         System.out.println("Starting the Spring Boot application...");
         SpringApplication.run(Main.class, args);
     }
